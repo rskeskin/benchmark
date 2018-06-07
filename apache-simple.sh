244990837935eafc02d0354f5b356c35ee3886d7
kerneltraceroute <1964547+kerneltraceroute@users.noreply.github.com>
2018-06-07 15:18:53 +0300

    Update apache-simple.sh

--- apache-simple.sh
+++ apache-simple.sh
@@ -8,22 +8,21 @@ HTTP="http"
 PAGE="index.html"
 
 TESTNAME="apache-simple-4vcpu"
-PHYTHREAD=4
+PHYTHREAD=4 #number of cores in the system
 
 
-DURATION=60
-TPOLL=5
-SNMPCOUNT=$(( DURATION / TPOLL ))
-RUNCOUNT=5
-CLIENTCOUNT=512
-CLIENTSTEP=64
+DURATION=60 #estimated value
+TPOLL=5 #estimated value
+SNMPCOUNT=$(( DURATION / TPOLL )) #number of samples received in DURATION Time
+RUNCOUNT=5 #
+THREADCOUNT=512
+THREADSTEP=64
 CORES=`snmpwalk -v2c -cpublic ${TARGET} 1.3.6.1.2.1.25.3.3.1.2 | wc -l`
 snmpwalk -v2c -cpublic -Oq ${TARGET} .1.3.6.1.4.1.2021.13.15.1.1.2
 read -p 'Specify block number : ' DISKNUM
 snmpwalk -v2c -cpublic -Oq ${TARGET} .1.3.6.1.2.1.31.1.1.1.1
 read -p 'Specify interface number : ' IFNUM
 mkdir -p -- "${TESTNAME}"
-
 #### FOR X'TH RUN ####
 RUNID=1
 while [ ${RUNID} -le ${RUNCOUNT} ]
@@ -37,16 +36,16 @@ do
     echo "################ RUN: $RUNID ################" >>${TESTNAME}/usage-neti.txt 2>&1
     echo "################ RUN: $RUNID ################" >>${TESTNAME}/usage-neto.txt 2>&1
     #### FOR X CLIENTS ####
-    CLIENT=${CLIENTSTEP}
-    while [ ${CLIENT} -le ${CLIENTCOUNT} ]
+    THREAD=${THREADSTEP}
+    while [ ${THREAD} -le ${THREADCOUNT} ]
     do
-        echo "######## CLIENTS: $CLIENT ########" >>${TESTNAME}/results.txt 2>&1
-        echo "######## CLIENTS: $CLIENT ########" >>${TESTNAME}/usage-cpui.txt 2>&1
-        echo "######## CLIENTS: $CLIENT ########" >>${TESTNAME}/usage-ramf.txt 2>&1
-        echo "######## CLIENTS: $CLIENT ########" >>${TESTNAME}/usage-disr.txt 2>&1
-        echo "######## CLIENTS: $CLIENT ########" >>${TESTNAME}/usage-disw.txt 2>&1
-        echo "######## CLIENTS: $CLIENT ########" >>${TESTNAME}/usage-neti.txt 2>&1
-        echo "######## CLIENTS: $CLIENT ########" >>${TESTNAME}/usage-neto.txt 2>&1
+        echo "######## CLIENTS: $THREAD ########" >>${TESTNAME}/results.txt 2>&1
+        echo "######## CLIENTS: $THREAD ########" >>${TESTNAME}/usage-cpui.txt 2>&1
+        echo "######## CLIENTS: $THREAD ########" >>${TESTNAME}/usage-ramf.txt 2>&1
+        echo "######## CLIENTS: $THREAD ########" >>${TESTNAME}/usage-disr.txt 2>&1
+        echo "######## CLIENTS: $THREAD ########" >>${TESTNAME}/usage-disw.txt 2>&1
+        echo "######## CLIENTS: $THREAD ########" >>${TESTNAME}/usage-neti.txt 2>&1
+        echo "######## CLIENTS: $THREAD ########" >>${TESTNAME}/usage-neto.txt 2>&1
         #### SNMPGET ( DURATION / POLL ) TIMES ####
         X=1
 
@@ -67,7 +66,7 @@ do
             .1.3.6.1.4.1.2021.13.15.1.1.13.${DISKNUM} \
             .1.3.6.1.2.1.31.1.1.1.6.${IFNUM} \
             .1.3.6.1.2.1.31.1.1.1.10.${IFNUM}))
-	    echo "${b[@]}" >>snmpdump.txt
+	    echo "${b[a]}" >>snmpdump.txt
             echo "cpu util"    `echo "scale=2; 100 - ((${b[0]} - ${a[0]}) / ($CORES * $TPOLL))" | bc` "%"                                  >>${TESTNAME}/usage-cpui.txt 2>&1
             echo "ram free" ${b[1]} "kB"                                                                                                   >>${TESTNAME}/usage-ramf.txt 2>&1
             echo "disk read"   `echo "x = (${b[2]} - ${a[2]}); if ( x < 0) (18446744073709551616 + x)/$TPOLL else (x)/$TPOLL" | bc` "B/s"  >>${TESTNAME}/usage-disr.txt 2>&1
@@ -79,9 +78,9 @@ do
         done &
 
         #### RUN WRK ####
-        wrk -H 'Connection: close' -c${CLIENT} -d${DURATION}s -t${PHYTHREAD} ${HTTP}://${TARGET}:${PORT}/${PAGE} >>${TESTNAME}/results.txt 2>&1
+        wrk -H 'Connection: close' -c${THREAD} -d${DURATION}s -t${PHYTHREAD} ${HTTP}://${TARGET}:${PORT}/${PAGE} >>${TESTNAME}/results.txt 2>&1
         wait
-        CLIENT=$(( CLIENT + CLIENTSTEP ))
+        THREAD=$(( THREAD + THREADSTEP ))
     done
     RUNID=$(( RUNID + 1 ))
 done
